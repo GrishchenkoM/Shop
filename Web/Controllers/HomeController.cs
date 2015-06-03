7cad9462c2cfc4544c4a4287508f99867ae0bad2
Michael <grishchenko.m@yandex.ru>
2015-06-03 19:40:42 +0300

    Create Projects

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using BusinessLogic;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        //public HomeController(DataManager dataManager)
+        //{
+        //    _dataManager = dataManager;
+        //}
+
+        public ActionResult Index()
+        {
+            ViewBag.CustomerId = 1;
+            return View();
+        }
+
+    //    [HttpPost]
+    //    public ActionResult Index(DataManager dataManager)
+    //    {
+            
+    //        ViewBag.CustomerId = 1;
+    //        return View(_dataManager.Customers.GetCustomerById(1));
+    //    }
+    }
+}
