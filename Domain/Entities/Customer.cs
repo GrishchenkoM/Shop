fb3c8b6a9e6237628f325084fe3875aee9570e3e
Michael <grishchenko.m@yandex.ru>
2015-06-03 21:51:19 +0300

    Modified Customer

--- Domain/Entities/Customer.cs
+++ Domain/Entities/Customer.cs
@@ -1,11 +1,16 @@
-﻿using Domain.Entities.Interfaces;
+﻿using System;
+using Domain.Entities.Interfaces;
 
 namespace Domain.Entities
 {
     public class Customer : ICustomer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
         public string Addres { get; set; }
         public string Sex { get; set; }
         public string Phone { get; set; }
--- Domain/Entities/Interfaces/ICustomer.cs
+++ Domain/Entities/Interfaces/ICustomer.cs
@@ -1,9 +1,15 @@
-﻿namespace Domain.Entities.Interfaces
+﻿using System;
+
+namespace Domain.Entities.Interfaces
 {
     public interface ICustomer
     {
         int Id { get; set; }
-        string Name { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string UserName { get; set; }
+        string Password { get; set; }
+        DateTime CreatedDate { get; set; }
         string Addres { get; set; }
         string Sex { get; set; }
         string Phone { get; set; }
