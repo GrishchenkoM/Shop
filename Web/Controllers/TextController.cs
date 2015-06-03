3592b9ef978310365b8fdcd98fbc07732d80e0e5
Michael <grishchenko.m@yandex.ru>
2015-06-03 19:48:49 +0300

    Create Text controller & view

---
+++ Web/Controllers/TextController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLogic;
+
+namespace Web.Controllers
+{
+    public class TextController : Controller
+    {
+        private DataManager _dataManager;
+
+        public TextController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
