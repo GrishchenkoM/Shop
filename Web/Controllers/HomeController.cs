395b42d42118c2124d256eb9b588278d2aab560b
Michael <grishchenko.m@yandex.ru>
2015-06-04 16:08:35 +0300

    home controller is authorized

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,29 +1,14 @@
 ﻿using System.Web.Mvc;
-using BusinessLogic;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        
-
-        //public HomeController(DataManager dataManager)
-        //{
-        //    _dataManager = dataManager;
-        //}
-
+        [Authorize]
         public ActionResult Index()
         {
             ViewBag.CustomerId = 1;
             return View();
         }
-
-    //    [HttpPost]
-    //    public ActionResult Index(DataManager dataManager)
-    //    {
-            
-    //        ViewBag.CustomerId = 1;
-    //        return View(_dataManager.Customers.GetCustomerById(1));
-    //    }
     }
 }
