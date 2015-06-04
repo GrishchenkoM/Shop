fa50a8a9264887a1b4d846825f8a1afb329c918c
Michael <grishchenko.m@yandex.ru>
2015-06-04 15:43:56 +0300

    Create Account controller & view

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLogic;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController(DataManager manager)
+        {
+            _dataManager = manager;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        private DataManager _dataManager;
+    }
+}
