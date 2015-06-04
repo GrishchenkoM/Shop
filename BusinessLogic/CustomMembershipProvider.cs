309401290fe0449975d1acab044b1283ba7ad670
Michael <grishchenko.m@yandex.ru>
2015-06-04 15:23:56 +0300

    Add Ninject library

--- BusinessLogic/CustomMembershipProvider.cs
+++ BusinessLogic/CustomMembershipProvider.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Web.Security;
 using BusinessLogic.Repositories.Interfaces;
+using Ninject;
 
 namespace BusinessLogic
 {
