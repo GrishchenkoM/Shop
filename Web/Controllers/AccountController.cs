685a9c98dfa83c7630517d321e0b1dd6b9d7ba55
Michael <grishchenko.m@yandex.ru>
2015-06-04 18:34:39 +0300

    Create HttpPost in Account controller

--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Security;
 using BusinessLogic;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -14,10 +12,24 @@ namespace Web.Controllers
             _dataManager = manager;
         }
 
-
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_dataManager.Provider.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Неудачная попытка входа на сайт");
+            }
+            return View(model);
         }
 
 
