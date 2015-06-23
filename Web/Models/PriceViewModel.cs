88f8125f98426ffe67ca002310eba134ccf8a016
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:52:41 +0300

    CustomerView Model and PriceView Model

---
+++ Web/Models/PriceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Web.Models
+{
+    public class PriceViewModel
+    {
+        public IEnumerable<SelectListItem> Price { get; set; }
+        public IEnumerable<int> SelectedPrice { get; set; }
+    }
+}
