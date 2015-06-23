7d8b6adde5081b1864d3c82e5915577719ba8bea
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:35:43 +0300

    New Db class for general methods. static ConnectionString method

---
+++ Domain/Db.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace Domain
+{
+    public class Db
+    {
+        public static string ConnectionString
+        {
+            get { return ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString; }
+        }
+    }
+}
