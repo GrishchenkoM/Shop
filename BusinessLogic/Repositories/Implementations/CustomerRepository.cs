dacf65eb3a4c1a2adad610d7d627b5c67d752a47
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:41:14 +0300

    big modification of CustomerRepository methods

--- BusinessLogic/Repositories/Implementations/CustomerRepository.cs
+++ BusinessLogic/Repositories/Implementations/CustomerRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
+using System.Data.SqlClient;
 using System.Web.Security;
 using BusinessLogic.Repositories.Interfaces;
 using Domain;
@@ -16,54 +15,64 @@ namespace BusinessLogic.Repositories.Implementations
         {
             _context = dbDataContext;
         }
-
-        public ICustomer GetCustomerById(int customerId)
-        {
-            return _context.Customers.FirstOrDefault(x => x.Id == customerId);
-        }
-
-        //public IEnumerable<ICustomer> GetCustomersByProduct(int productId)
-        //{
-        //    return _context.Customers.Where();
-        //}
-
+        
         public IEnumerable<ICustomer> GetCustomers()
         {
-            return _context.Customers;
+            const string query = "select * from Customers";
+            return ExecuteQuery.GetCustomers(_context, query);
         }
-        public ICustomer GetCustomerByName(string userName)
-        {
-            return _context.Customers.FirstOrDefault(x => x.UserName == userName);
-        }
-        public void CreateCustomer(string userName, string password, string firstName, string lastName)
+        
+        public void CreateCustomer(string userName, string password, string firstName, string lastName, string email)
         {
             var customer = new Customer
                 {
                     UserName = userName,
                     Password = password,
                     FirstName = firstName,
-                    LastName = lastName
+                    LastName = lastName,
+                    Addres = "",
+                    CreatedDate = DateTime.Now,
+                    Email = email,
+                    Id = -1,
+                    Phone = "",
+                    Sex = ""
                 };
 
             SaveCustomer(customer);
         }
         public bool ValidateCustomer(string userName, string password)
         {
-            var customer = _context.Customers.FirstOrDefault(x => x.UserName == userName);
-            if (customer != null && customer.Password == password) return true;
+            if (_context != null)
+            {
+                //var customer = _context.Customers.FirstOrDefault(x => x.UserName == userName);
+                var customer = GetCustomerByName(userName);
+                if (customer != null && customer.Password == password) return true;
+            }
             return false;
         }
         public void SaveCustomer(ICustomer customer)
         {
-            if (customer.Id == 1)
-                _context.Customers.Add((Customer)customer);
-            else
-                _context.Entry(customer).State = EntityState.Modified;
-            _context.SaveChanges();
+            using (var connection = new SqlConnection(_context.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO Customers" +
+                                          "(FirstName, LastName, UserName, Password, Email, CreatedDate, Address, Sex, Phone)" +
+                                          "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', CAST('{5}' AS datetime2), '{6}', '{7}', '{8}')";
+                    command.CommandText = string.Format(command.CommandText,
+                        customer.FirstName, customer.LastName, customer.UserName, customer.Password,
+                        customer.Email, customer.CreatedDate, customer.Addres, customer.Sex, customer.Phone);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
         }
         public MembershipUser GetMembershipCustomerByName(string userName)
         {
-            var customer = _context.Customers.FirstOrDefault(x => x.UserName == userName);
+            //var customer = _context.Customers.FirstOrDefault(x => x.UserName == userName);
+            ICustomer customer = GetCustomerByName(userName);
             if (customer != null)
                 return new MembershipUser(
                     "CustomMembershipProvider",
@@ -85,10 +94,63 @@ namespace BusinessLogic.Repositories.Implementations
 
         public ICustomer GetCustomerByEmail(string email)
         {
-            return _context.Customers.FirstOrDefault(x => x.Email == email);
+            //return _context.Customers.FirstOrDefault(x => x.Email == email);
+
+            string query = @"select * from Customers where Email = '{0}'";
+            query = string.Format(query, email);
+
+            return GetCustomer(query);
         }
+        public ICustomer GetCustomerByName(string userName)
+        {
+            //return _context.Customers.FirstOrDefault(x => x.UserName == userName);
+
+            string query = @"select * from Customers where UserName = '{0}'";
+            query = string.Format(query, userName);
+
+            return ExecuteQuery.GetCustomer(_context, query);
+        }
+        public ICustomer GetCustomerById(int customerId)
+        {
+            //return _context.Customers.FirstOrDefault(x => x.Id == customerId);
+
+            string query = @"select * from Customers where Id = '{0}'";
+            query = string.Format(query, customerId);
+
+            return GetCustomer(query);
+        }
+        private ICustomer GetCustomer(string query)
+        {
+            using (var connection = new SqlConnection(_context.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = query;
+                    SqlDataReader reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    ICustomer customer = null;
+                    if (reader.Read())
+                    {
+                        customer = new Customer();
+                        customer.FirstName = reader.GetString(1);
+                        customer.LastName = reader.GetString(2);
+                        customer.UserName = reader.GetString(3);
+                        customer.Password = reader.GetString(4);
+                        customer.Email = reader.GetString(5);
+                        if (reader.GetValue(6) != DBNull.Value)
+                            customer.CreatedDate = (DateTime)reader.GetValue(6);
+                        customer.Addres = reader.GetValue(7) != DBNull.Value ? reader.GetString(7) : "";
+                        customer.Sex = reader.GetValue(8) != DBNull.Value ? reader.GetString(8) : "";
+                        customer.Phone = reader.GetValue(9) != DBNull.Value ? reader.GetString(9) : "";
+                    }
+                    return customer;
+                }
+            }
+        }
+        
 
 
         private readonly DbDataContext _context;
     }
 }
+
