20a0aec67fca651b42b169e888e8a331cded1400
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:40:20 +0300

    new repository of IProductsCustomers

---
+++ BusinessLogic/Repositories/Implementations/ProductsCustomersRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogic.Repositories.Interfaces;
+using Domain;
+using Domain.Entities.Interfaces;
+
+namespace BusinessLogic.Repositories.Implementations
+{
+    public class ProductsCustomersRepository : IProductsCustomersRepository
+    {
+        public ProductsCustomersRepository(DbDataContext dbDataContext)
+        {
+            _context = dbDataContext;
+        }
+
+        public IProductsCustomers GetProductsCustomersById(int productsCustomersId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICustomer GetCustomerByProduct(int productId)
+        {
+            string query = "";
+            return ExecuteQuery.GetCustomer(_context, query);
+        }
+
+        public IEnumerable<IProduct> GetProductsByCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IProductsCustomers> GetProductsCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private readonly DbDataContext _context;
+    }
+}
