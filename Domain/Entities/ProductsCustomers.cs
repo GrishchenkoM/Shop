f27e281445925ac1df26acb2cf1783ae0dc1a63e
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:32:55 +0300

    New entity ProductsCustomers for connection between Products and Customers. main field: Count (products count of customer)

---
+++ Domain/Entities/ProductsCustomers.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities.Interfaces;
+
+namespace Domain.Entities
+{
+    public class ProductsCustomers : IProductsCustomers
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
