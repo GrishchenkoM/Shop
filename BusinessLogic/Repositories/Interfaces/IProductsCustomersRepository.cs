9852502dd8a378e384edfe37d4eef73082087142
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:39:57 +0300

    new repository of IProductsCustomers

---
+++ BusinessLogic/Repositories/Interfaces/IProductsCustomersRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Domain.Entities.Interfaces;
+
+namespace BusinessLogic.Repositories.Interfaces
+{
+    public interface IProductsCustomersRepository
+    {
+        IProductsCustomers GetProductsCustomersById(int productsCustomersId);
+        ICustomer GetCustomerByProduct(int productId);
+        IEnumerable<IProduct> GetProductsByCustomer(int customerId);
+        IEnumerable<IProductsCustomers> GetProductsCustomers();
+    }
+}
