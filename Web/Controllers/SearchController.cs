2644df06b81a498803d7ad4a84f47d5e8cc15e70
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:50:30 +0300

    SearchController: Index

---
+++ Web/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using BusinessLogic;
+using Domain.Entities;
+
+namespace Web.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        public SearchController(DataManager dataManager)
+        {
+            _manager = dataManager;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(string keyword)
+        {
+            IEnumerable<Product> model =
+                (IEnumerable<Product>)
+                _manager.Products.GetProducts()
+                        .Where(x => x.Name.ToLowerInvariant().StartsWith(keyword.ToLowerInvariant()));
+            return View(model);
+        }
+
+        private DataManager _manager;
+    }
+}
