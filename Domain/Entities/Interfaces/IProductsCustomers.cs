e83d1800e03b4320a44ff22d1da371867d3e2d2b
Michael <grishchenko.m@yandex.ru>
2015-06-23 11:33:21 +0300

    interface of new entity ProductsCustomers

---
+++ Domain/Entities/Interfaces/IProductsCustomers.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities.Interfaces
+{
+    public interface IProductsCustomers
+    {
+        int Id { get; set; }
+        int CustomerId { get; set; }
+        int ProductId { get; set; }
+        int Count { get; set; }
+    }
+}
