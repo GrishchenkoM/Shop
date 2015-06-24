8e0d10e53ec8b98ca13febf83fd6613f8fa68c3c
Michael <grishchenko.m@yandex.ru>
2015-06-24 13:55:03 +0300

    modified AddProduct() to ChangeProduct()

--- BusinessLogic/Repositories/Implementations/ProductRepository.cs
+++ BusinessLogic/Repositories/Implementations/ProductRepository.cs
@@ -39,14 +39,36 @@ namespace BusinessLogic.Repositories.Implementations
         {
             string query = "INSERT INTO Products " +
                            "(Name, IsAvailable, Cost, Image, Description) " +
-                           "VALUES ('{0}', {1}, {2}, @binaryValue, '{3}')";
+                           "VALUES ('{0}', {1}, CAST('{2}' AS money), @binaryValue, '{3}')";
             query = string.Format(query,
                           product.Name,
                           Convert.ToSByte(product.IsAvailable),
                           product.Cost,
                           product.Description);
 
-            return ExecuteQuery.AddProduct(_context, query, (Object)product.Image);
+            return ExecuteQuery.ChangeProduct(_context, query, (Object)product.Image);
+        }
+
+        public bool UpdateProduct(IProduct item)
+        {
+            string query = "UPDATE Products " +
+                           "SET Name = '{0}', IsAvailable = {1}, Cost = CAST('{2}' AS money), Image = @binaryValue, Description = '{3}' " +
+                           "WHERE Products.Id = {4}";
+            query = string.Format(query,
+                          item.Name,
+                          Convert.ToSByte(item.IsAvailable),
+                          item.Cost,
+                          item.Description,
+                          item.Id);
+
+            return ExecuteQuery.ChangeProduct(_context, query, (Object)item.Image);
+        }
+        public bool DeleteProduct(int id)
+        {
+            string query = "DELETE FROM Products WHERE Products.Id = {0}";
+            query = string.Format(query, id);
+
+            return ExecuteQuery.ChangeProduct(_context, query);
         }
 
         private readonly DbDataContext _context;
