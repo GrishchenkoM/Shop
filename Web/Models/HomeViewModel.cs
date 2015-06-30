9c9e2313dbdab0000f11f04a3cddc606d28cc4ca
Michael <grishchenko.m@yandex.ru>
2015-06-30 16:30:40 +0300

    home View Model

---
+++ Web/Models/HomeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain.Entities.Interfaces;
+
+namespace Web.Models
+{
+    public class HomeViewModel
+    {
+        //public int ProductId { get; private set; }
+        //public string ProductName { get; private set; }
+        //public byte[] ProductImage { get; private set; }
+        //public string ProductDescription { get; private set; }
+        //public decimal ProductCost { get; private set; }
+        //public bool IsAvailable { get; private set; }
+        public List<IProduct> Products { get; set; }
+        public List<IProduct> PopularProducts { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
