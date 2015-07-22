0439a26d809ca173a2866650fc7e1b4b1917d826
Michael <grishchenko.m@yandex.ru>
2015-07-22 17:55:23 +0300

    new About Site

---
+++ Web/Controllers/AboutSiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    [HandleError(ExceptionType = typeof(Exception), View = "Pity")]
+    public class AboutSiteController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["UserId"] != null)
+                ViewBag.CustomerId = (int) Session["UserId"];
+            return View();
+        }
+
+    }
+}
