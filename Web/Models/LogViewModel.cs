e96a9b8fc3baa22383ebdddc2167d7b7751138ad
Michael <grishchenko.m@yandex.ru>
2015-07-22 17:57:16 +0300

    new Log View Model

---
+++ Web/Models/LogViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web.Models
+{
+    public class LogViewModel
+    {
+        public List<LogItem> ItemsSold { get; set; }
+        public List<LogItem> ItemsBought { get; set; }
+        public int ApproximateAmount { get; set; }
+        public int CustomerId { get; set; }
+    }
+
+    public class LogItem
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int ProductId { get; set; }
+        public byte[] ProductImage { get; set; }
+        public string ProductName { get; set; }
+        public int Count { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool IsMine { get; set; }
+    }
+}
