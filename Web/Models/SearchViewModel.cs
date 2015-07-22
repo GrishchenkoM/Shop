0faa3e626b13eaa1aaaeb135deaf8e8eb03f71a5
Michael <grishchenko.m@yandex.ru>
2015-07-22 17:57:26 +0300

    new Search View Model

---
+++ Web/Models/SearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain.Entities.Interfaces;
+
+namespace Web.Models
+{
+    public class SearchViewModel
+    {
+        public List<IProduct> SearchResultList { get; set; }
+    }
+}
