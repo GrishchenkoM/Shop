64717080160f74520c728baceb625d4ecd239f8d
Michael <grishchenko.m@yandex.ru>
2015-07-22 17:08:50 +0300

    add ajax

--- Web/Controllers/FindController.cs
+++ Web/Controllers/FindController.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web.Mvc;
 using BusinessLogic;
 using Domain.Entities.Interfaces;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -15,19 +18,39 @@ namespace Web.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.SearchString = "";
             return View();
         }
-        [HttpPost]
-        public ActionResult Index(string name)
+        [HttpPost, HandleError(ExceptionType = typeof(Exception), View = "Pity")]
+        public ActionResult Index(string searchString)
         {
-            IEnumerable<IProduct> model;
-            if (name == "")
-                model = _dataManager.Products.GetProducts();
-            else
-                model = _dataManager.Products.GetProducts().Where(
-                    x => x.Name.ToLowerInvariant().StartsWith(name.ToLowerInvariant()));
+            ViewBag.SearchString = searchString;
+            return View();
+        }
 
-            return View(model);
+        public ActionResult GetFoundProducts(string searchString)
+        {
+            IEnumerable<IProduct> products;
+            if (searchString == "")
+                products = _dataManager.Products.GetProducts();
+            else
+                products = _dataManager.Products.GetProducts().Where(
+                    x => x.Name.ToLowerInvariant().Contains(searchString.ToLowerInvariant()));
+
+            var productsCustomers = _dataManager.ProductsCustomers.GetProductsCustomers();
+
+            if (products == null || productsCustomers == null) return RedirectToAction("Index", "Find");
+
+            var model = new SearchViewModel {SearchResultList = new List<IProduct>()};
+
+
+            model.SearchResultList = (from product in products
+                                          join prodCust in productsCustomers
+                                              on product.Id equals prodCust.ProductId
+                                          where prodCust.CustomerId == (int)Session["UserId"]
+                                          select product).ToList();
+
+            return View("GetProducts",model);
         }
 
         private readonly DataManager _dataManager;
--- Web/Models/HomeViewModel.cs
+++ Web/Models/HomeViewModel.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using Domain.Entities.Interfaces;
 
 namespace Web.Models
 {
     public class HomeViewModel
     {
-        //public int ProductId { get; private set; }
-        //public string ProductName { get; private set; }
-        //public byte[] ProductImage { get; private set; }
-        //public string ProductDescription { get; private set; }
-        //public decimal ProductCost { get; private set; }
-        //public bool IsAvailable { get; private set; }
         public List<IProduct> Products { get; set; }
         public List<IProduct> PopularProducts { get; set; }
         public int CustomerId { get; set; }
+        public string SearchString { get; set; }
     }
 }
