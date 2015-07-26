1c2d594a5b14f63c693515c789b81d63754da5b6
Michael <grishchenko.m@yandex.ru>
2015-07-26 23:10:33 +0300

    Full refactoring in Home Controller

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -13,6 +13,8 @@ namespace Web.Controllers
     [HandleError(ExceptionType = typeof(Exception), View = "Pity")]
     public class HomeController : Controller
     {
+        #region public
+
         public HomeController(DataManager manager)
         {
             _dataManager = manager;
@@ -20,126 +22,13 @@ namespace Web.Controllers
 
         public ActionResult Index(int id = -1)
         {
-            
+
             HomeViewModel model = null;
 
             CreateModel(ref model, id);
 
             ViewBag.SearchString = model.SearchString;
             return View(model);
-        }
-
-        private void CreateModel(ref HomeViewModel model, int id = -1)
-        {
-            if (Session["HomeViewModel"] == null &&
-                Session["UserId"] == null && id == -1)
-            {
-                Session.Add("UserId", id);
-                model = new HomeViewModel {CustomerId = id};
-                Session.Add("HomeViewModel", model);
-            }
-            else if (Session["HomeViewModel"] == null &&
-                     Session["UserId"] == null && id != -1)
-            {
-                Session.Add("UserId", id);
-                model = new HomeViewModel {CustomerId = id};
-                Session.Add("HomeViewModel", model);
-            }
-
-            else if (Session["HomeViewModel"] == null &&
-                     Session["UserId"] != null && id == -1)
-            {
-                if ((int) Session["UserId"] != -1)
-                    id = (int) Session["UserId"];
-
-                model = new HomeViewModel {CustomerId = id};
-                Session.Add("HomeViewModel", model);
-            }
-            else if (Session["HomeViewModel"] == null &&
-                     Session["UserId"] != null && id != -1)
-            {
-                if ((int) Session["UserId"] != id)
-                    Session["UserId"] = id;
-
-                model = new HomeViewModel {CustomerId = id};
-                Session.Add("HomeViewModel", model);
-            }
-
-            else if (Session["HomeViewModel"] != null &&
-                     Session["UserId"] == null && id == -1)
-            {
-                model = Session["HomeViewModel"] as HomeViewModel ?? new HomeViewModel {CustomerId = id};
-                Session.Add("UserId", model.CustomerId);
-            }
-            else if (Session["HomeViewModel"] != null &&
-                     Session["UserId"] == null && id != -1)
-            {
-                model = Session["HomeViewModel"] as HomeViewModel ?? new HomeViewModel {CustomerId = id};
-                Session.Add("UserId", model.CustomerId);
-            }
-
-            else if (Session["HomeViewModel"] != null &&
-                     Session["UserId"] != null && id == -1)
-            {
-                model = Session["HomeViewModel"] as HomeViewModel ?? new HomeViewModel {CustomerId = id};
-                if ((int) Session["UserId"] != -1)
-                    id = (int) Session["UserId"];
-                model.CustomerId = id;
-            }
-            else if (Session["HomeViewModel"] != null &&
-                     Session["UserId"] != null && id != -1)
-            {
-                model = Session["HomeViewModel"] as HomeViewModel ?? new HomeViewModel {CustomerId = id};
-
-                if ((int) Session["UserId"] != id)
-                    Session["UserId"] = id;
-
-                if (model.CustomerId <= 0)
-                    model.CustomerId = (int) Session["UserId"];
-            }
-
-
-            IEnumerable<IProductsCustomers> productsCustomers = _dataManager.ProductsCustomers.GetProductsCustomers();
-            IEnumerable<IProduct> products = _dataManager.Products.GetProducts();
-            IEnumerable<IOrder> orders = _dataManager.Orders.GetOrders();
-
-            if (productsCustomers != null && products != null)
-            {
-                var innerJoinQuery =
-                    from prod in products
-                    join prodCust in productsCustomers
-                        on prod.Id equals prodCust.ProductId
-                    where prodCust.Count > 0
-                    orderby prod.Id descending
-                    select prod;
-                model.Products = new List<IProduct>();
-                foreach (var product in innerJoinQuery)
-                {
-                    model.Products.Add(product);
-                }
-
-                // чаще всего покупается. если нет - последний добавленый
-                if (orders != null)
-                {
-                    innerJoinQuery =
-                        (from prod in products
-                         join order in orders
-                             on prod.Id equals order.ProductId
-                         join prodCust in productsCustomers
-                             on prod.Id equals prodCust.ProductId
-                         where prodCust.Count > 0
-                         orderby order.Count descending
-                         select prod);
-
-                    var distinctInnerJoinQuery = innerJoinQuery.Distinct();
-
-                    model.PopularProducts = new List<IProduct>();
-                    foreach (var product in distinctInnerJoinQuery)
-                    {
-                        model.PopularProducts.Add(product);
-                    }
-                }
-            }
         }
 
         [HttpPost]
@@ -150,7 +39,7 @@ namespace Web.Controllers
             ViewBag.SearchString = tempSearchString;
             return View(model);
         }
-        
+
         public ActionResult GetFoundProducts(string searchString)
         {
             IEnumerable<IProduct> products = null;
@@ -161,16 +50,158 @@ namespace Web.Controllers
 
             SearchViewModel model = null;
             if (products != null)
-            {
-                model = new SearchViewModel {SearchResultList = new List<IProduct>()};
-                foreach (var product in products)
-                    model.SearchResultList.Add(product);
-            }
+                SetSearchResultListModel(ref model, products);
+
             return View("GetFoundProducts", model);
         }
+
+        #endregion
+
+        #region private
+
+        private void CreateModel(ref HomeViewModel model, int id = -1)
+        {
+            ModelAndUserIdSetUp(ref model, id);
+            SetContentInModel(model);
+        }
+
+        private void ModelAndUserIdSetUp(ref HomeViewModel model, int id)
+        {
+            if (!HomeViewModelSessionExist() && !UserIdSessionExist())
+            {
+                SetUserId(id);
+                SetHomeViewModel(ref model, id);
+            }
+            else if (!HomeViewModelSessionExist() && UserIdSessionExist() &&
+                     ((id == -1 && UserIdSession != -1) ||
+                      (id != -1 && UserIdSession != id)))
+            {
+                if (UserIdSession != -1) SetId(out id);
+                else if (UserIdSession != id) SetUserId(id);
+
+                SetHomeViewModel(ref model, id);
+            }
+            else if (HomeViewModelSessionExist() && !UserIdSessionExist())
+            {
+                SetHomeViewModelAs(out model, id);
+                SetUserId(model.CustomerId);
+            }
+            else if (HomeViewModelSessionExist() && UserIdSessionExist())
+            {
+                SetHomeViewModelAs(out model, id);
+
+                if (id == -1)
+                {
+                    if (UserIdSession != -1) SetId(out id);
+                    model.CustomerId = id;
+                }
+                else
+                {
+                    if (UserIdSession != id) SetUserId(id);
+                    if (model.CustomerId <= 0)
+                        model.CustomerId = UserIdSession;
+                }
+            }
+        }
+
+        private void SetContentInModel(HomeViewModel model)
+        {
+            IEnumerable<IProductsCustomers> productsCustomers = _dataManager.ProductsCustomers.GetProductsCustomers();
+            IEnumerable<IProduct> products = _dataManager.Products.GetProducts();
+            IEnumerable<IOrder> orders = _dataManager.Orders.GetOrders();
+
+            if (productsCustomers != null && products != null)
+            {
+                SetModelProducts(model, products, productsCustomers);
+
+                if (orders != null)
+                    SetModelPopularProducts(model, products, orders, productsCustomers);
+            }
+        }
+
+        private void SetModelPopularProducts(HomeViewModel model, IEnumerable<IProduct> products, IEnumerable<IOrder> orders,
+                                             IEnumerable<IProductsCustomers> productsCustomers)
+        {
+            var innerJoinQuery =
+                (from prod in products
+                 join order in orders
+                     on prod.Id equals order.ProductId
+                 join prodCust in productsCustomers
+                     on prod.Id equals prodCust.ProductId
+                 where prodCust.Count > 0
+                 orderby order.Count descending
+                 select prod);
+
+            var distinctInnerJoinQuery = innerJoinQuery.Distinct();
+
+            model.PopularProducts = new List<IProduct>();
+            foreach (var product in distinctInnerJoinQuery)
+            {
+                model.PopularProducts.Add(product);
+            }
+        }
+
+        private void SetModelProducts(HomeViewModel model, IEnumerable<IProduct> products, IEnumerable<IProductsCustomers> productsCustomers)
+        {
+            var innerJoinQuery =
+                from prod in products
+                join prodCust in productsCustomers
+                    on prod.Id equals prodCust.ProductId
+                where prodCust.Count > 0
+                orderby prod.Id descending
+                select prod;
+            model.Products = new List<IProduct>();
+            foreach (var product in innerJoinQuery)
+            {
+                model.Products.Add(product);
+            }
+        }
         
-        public int Id { get; private set; }
+        private void SetId(out int id)
+        {
+            id = (int) Session["UserId"];
+        }
+
+        private void SetUserId(int id)
+        {
+            Session["UserId"] = id;
+        }
+
+        private void SetHomeViewModelAs(out HomeViewModel model, int id)
+        {
+            model = Session["HomeViewModel"] as HomeViewModel ?? new HomeViewModel { CustomerId = id };
+        }
+
+        private void SetHomeViewModel(ref HomeViewModel model, int id)
+        {
+            model = new HomeViewModel {CustomerId = id};
+            Session["HomeViewModel"] = model;
+        }
+
+        private void SetSearchResultListModel(ref SearchViewModel model, IEnumerable<IProduct> products)
+        {
+            model = new SearchViewModel {SearchResultList = new List<IProduct>()};
+            foreach (var product in products)
+                model.SearchResultList.Add(product);
+        }
+
+        private bool HomeViewModelSessionExist()
+        {
+            return Session["HomeViewModel"] != null;
+        }
+
+        private bool UserIdSessionExist()
+        {
+            return Session["UserId"] != null;
+        }
+        
+        private int UserIdSession
+        {
+            get { return (int)Session["UserId"]; }
+        }
 
         private readonly DataManager _dataManager;
+
+        #endregion
     }
 }
