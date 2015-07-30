f5a0c8cd232eb1163dee090410517dcaa0c09ee8
Michael <grishchenko.m@yandex.ru>
2015-07-30 14:15:10 +0300

    Create Auxiliary class for default methods

---
+++ Web/Models/Auxiliary.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Web;
+using Domain.Entities.Interfaces;
+
+namespace Web.Models
+{
+    public static class Auxiliary
+    {
+        /// <summary>
+        /// Read Image file
+        /// </summary>
+        /// <param name="uploadImage">Image file for upload</param>
+        /// <param name="item">Item wich has to contain the image file</param>
+        public static void ReadImage(HttpPostedFileBase uploadImage, object item)
+        {
+            if(item is IProduct)
+                (item as IProduct).Image = ReadImage(uploadImage);
+            else if(item is CreateProduct)
+                (item as CreateProduct).Image = ReadImage(uploadImage);
+        }
+        
+        private static byte[] ReadImage(HttpPostedFileBase uploadImage)
+        {
+            byte[] imageData;
+            using (var binaryReader = new BinaryReader(uploadImage.InputStream))
+                imageData = binaryReader.ReadBytes(uploadImage.ContentLength);
+            return imageData;
+        }
+
+        public enum Result
+        {
+            Error = -1, AdditionSuccess, OperationSuccess
+        }
+    }
+}
